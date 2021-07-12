a9d4e4e55011d7b37e0b4c54c41c3ad2e5ae1372
EF <erikfrits95@gmail.com>
2021-07-12 08:46:36 +0200

    :rocket: Snippets

--- lib/Snippets/selection.py
+++ lib/Snippets/selection.py
@@ -1,7 +1,14 @@
-from Autodesk.Revit.DB import ViewPlan, ViewSection, View3D,ViewSchedule, ViewDuplicateOption, Transaction, ViewType, View
+from Autodesk.Revit.DB import (ViewPlan, ViewSection,
+                               View3D,ViewSchedule, ViewDuplicateOption,
+                               Transaction, ViewType, View,
+                               FilteredElementCollector,
+                               BuiltInCategory,
+                               BuiltInParameter)
+from pyrevit.forms import SelectFromList
 from pyrevit import forms
 
-
+from Snippets.variables import ALL_VIEW_TYPES
+import sys
 
 uidoc = __revit__.ActiveUIDocument
 doc = __revit__.ActiveUIDocument.Document
@@ -21,3 +28,50 @@ def get_selected_elements(self):
         forms.alert("No elements  were selected.\nPlease, try again.", exitscript=True, title=__title__)
 
     return selected_elements
+
+
+
+
+
+
+
+
+def get_selected_views():
+    """Function to get selected views.
+    :return: list of selected views."""
+    # GET SELECTED ELEMENTS
+    UI_selected = uidoc.Selection.GetElementIds()
+
+    # FILTER SELECTION
+    selected_views = [doc.GetElement(view_id) for view_id in UI_selected if type(doc.GetElement(view_id)) in ALL_VIEW_TYPES]
+
+    return selected_views
+
+
+
+
+
+def select_title_block():
+    """Function to let user select a title block."""
+
+
+
+
+
+    # ______________________________ SELECT TITLE BLOCK
+    all_title_blocks = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_TitleBlocks).WhereElementIsElementType().ToElements()
+    unique_title_blocks = {}
+    for tb in all_title_blocks:
+        family_name = tb.FamilyName
+        type_name = tb.get_Parameter(BuiltInParameter.SYMBOL_NAME_PARAM).AsString()
+        unique_title_blocks["{} - {}".format(family_name, type_name)] = tb.Id
+
+    selected_option = SelectFromList.show(list(unique_title_blocks), title="TEST")
+    if not selected_option:
+        print("Nothing was selected")
+        print("Script was cancelled. Try again.")
+        sys.exit()
+    selected_title_block = unique_title_blocks[selected_option]
+
+
+    return selected_title_block
---
+++ lib/Snippets/variables.py
@@ -0,0 +1,10 @@
+from Autodesk.Revit.DB import (ViewPlan,
+                               ViewSection,
+                               View3D,
+                               ViewSchedule,
+                               View,
+                               ViewType,
+                               ViewDrafting)
+
+
+ALL_VIEW_TYPES = [ViewPlan, ViewSection, View3D , ViewSchedule, View, ViewDrafting]
