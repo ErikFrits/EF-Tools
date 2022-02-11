6c758fa1e332faf2e940f5dadd7c5f5b9b5318bb
EF <erikfrits95@gmail.com>
2022-02-11 08:42:18 +0100

    :rocket: lib updated
    - replaced DisplayUnitType with ForgeTypeId() for Revit 2022

--- lib/Snippets/_convert.py
+++ lib/Snippets/_convert.py
@@ -1,11 +1,35 @@
 # -*- coding: utf-8 -*-
 
 from Autodesk.Revit.DB import *
+app   = __revit__.Application
+from Autodesk.Revit.DB import UnitUtils
 
 def convert_cm_to_feet(length):
     """Function to convert cm to feet."""
-    return UnitUtils.Convert(length,
-                           DisplayUnitType.DUT_CENTIMETERS,
-                           DisplayUnitType.DUT_DECIMAL_FEET)
+    rvt_year = int(app.VersionNumber)
 
+    # RVT >= 2022
+    if rvt_year < 2022:
+        from Autodesk.Revit.DB import DisplayUnitType
+        return UnitUtils.Convert(length,
+                                DisplayUnitType.DUT_CENTIMETERS,
+                                DisplayUnitType.DUT_DECIMAL_FEET)
+    # RVT >= 2022
+    else:
+        from Autodesk.Revit.DB import UnitTypeId
+        return UnitUtils.ConvertToInternalUnits(length, UnitTypeId.Centimeters)
 
+def convert_m_to_feet(length):
+    """Function to convert cm to feet."""
+    rvt_year = int(app.VersionNumber)
+
+    # RVT >= 2022
+    if rvt_year < 2022:
+        from Autodesk.Revit.DB import DisplayUnitType
+        return UnitUtils.Convert(length,
+                                 DisplayUnitType.DUT_METERS,
+                                 DisplayUnitType.DUT_DECIMAL_FEET)
+    # RVT >= 2022
+    else:
+        from Autodesk.Revit.DB import UnitTypeId
+        return UnitUtils.ConvertToInternalUnits(length, UnitTypeId.Meters)
