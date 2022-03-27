844a84f1a2f428dab9e000c02a3b2b9834e7d36f
EF <erikfrits95@gmail.com>
2022-03-27 15:52:43 +0200

    :fire: Auto-Updated Removed.
    Auto-updated didn't work as expected so I am removing it from Extension and later on I will add a manual update button if I make it work as expected.

--- startup.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-doc = """This file will be executed everytime Revit starts."""
-import os
-
-# UPDATE EF-TOOLS
-cmd_command = 'cmd /c "pyrevit extensions update EF-Tools"'
-os.system(cmd_command)
