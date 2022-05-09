11458d3805d47ffcefe7b01de4efe873cd774531
EF <erikfrits95@gmail.com>
2022-05-09 22:23:36 +0200

    :package: Updated UI + Refactored
    Updated UI:
    RadioButtons -> Checkboxes with Single Selection
    Hyperlinks updated
    Refactored code a lot.

--- EF_Tools.tab/Elements.panel/col2.stack/Rooms.pulldown/RoomsToFloors.pushbutton/script.py
+++ EF_Tools.tab/Elements.panel/col2.stack/Rooms.pulldown/RoomsToFloors.pushbutton/script.py
@@ -1,61 +1,56 @@
 # -*- coding: utf-8 -*-
-__title__  = "Rooms to Floors"
-__author__ = "Erik Frits"
-__doc__ = """Version = 1.1
+__title__     = "Rooms to Floors"
+__author__    = "Erik Frits"
+__highlight__ = 'updated'
+__version__   = 'Version = 1.2'
+__doc__       = """Version = 1.2
 Date    = 29.07.2021
 _____________________________________________________________________
 Description:
 
 Create Floors from selected Rooms.
-If none selected it will promt user with a Yes/No dialog box 
+If no rooms selected it will promt user with a Yes/No dialog box 
 to select all visible Floors in an active view.
 _____________________________________________________________________
 How-to:
 
 -> Select rooms
 -> Run the script
--> If no rooms selected it ask to select all rooms in an active view.
+-> If no rooms selected it will ask to select all rooms in an active view.
 -> Select FloorType
 _____________________________________________________________________
 Last update:
+- [08.05.2022] - 1.2 RELEASE
+- [08.05.2022] - Updated GUI(Checkboxes + Filtering) + Refactoring
 - [16.11.2021] - 1.1 RELEASE
 - [16.11.2021] - GUI ADDED
 - [29.07.2021] - 1.0 RELEASE
 - [29.07.2021] - Refactored
-- [29.07.2021] - Solved Floors with openings 
+- [29.07.2021] - Bug Solved: Floors with openings 
 - [29.07.2021] - Select all rooms if none selected
 _____________________________________________________________________
-Author: Erik Frits
-"""
+Author: Erik Frits"""
 # ╦╔╦╗╔═╗╔═╗╦═╗╔╦╗╔═╗
 # ║║║║╠═╝║ ║╠╦╝ ║ ╚═╗
 # ╩╩ ╩╩  ╚═╝╩╚═ ╩ ╚═╝ IMPORTS
 #==================================================
 import sys
-from pyrevit import revit, forms
-from Snippets._selection import get_selected_rooms
-from Snippets._context_manager import ef_Transaction
-from Autodesk.Revit.DB import (BuiltInParameter,
-                               SpatialElementBoundaryOptions,
-                               FilteredElementCollector,
-                               ElementCategoryFilter,
-                               BuiltInCategory,
-                               CurveArray,
-                               TransactionGroup,
-                               Element,
-                               FloorType)
+from pyrevit import forms
+from Autodesk.Revit.DB import (FilteredElementCollector, ElementCategoryFilter,
+                               BuiltInCategory, BuiltInParameter,
+                               Element, FloorType, TransactionGroup,
+                               CurveArray,SpatialElementBoundaryOptions)
 
 #>>>>>>>>>> .NET IMPORTS
 import clr
-clr.AddReference("System.Windows.Forms")
-from System.Windows.Forms import (DialogResult,
-                                  MessageBox,
-                                  MessageBoxButtons)
 clr.AddReference("System")
-from System.Diagnostics.Process import Start
-from System.Collections.Generic import List
-from System.Windows.Window      import DragMove
-from System.Windows.Input       import MouseButtonState
+clr.AddReference("System.Windows.Forms")
+from System.Windows.Forms import DialogResult, MessageBox, MessageBoxButtons
+
+#Custom
+from Snippets._context_manager  import ef_Transaction, try_except
+from Snippets._selection        import get_selected_rooms
+from GUI.forms                  import select_from_dict
 
 # ╦  ╦╔═╗╦═╗╦╔═╗╔╗ ╦  ╔═╗╔═╗
 # ╚╗╔╝╠═╣╠╦╝║╠═╣╠╩╗║  ║╣ ╚═╗
@@ -78,176 +73,80 @@ def ask_select_all_floors():
     :return: List of all room visible in the currect view. if None - Exit Script"""
     # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> PROMT USER YES/NO
     dialogResult = MessageBox.Show('There were no rooms selected. Would you like to select all rooms visible in the active view?',
-                                    __title__,
-                                    MessageBoxButtons.YesNo)
-
+                                    __title__, MessageBoxButtons.YesNo)
     if (dialogResult == DialogResult.Yes):
         return FilteredElementCollector(doc, active_view_id).WherePasses(ElementCategoryFilter(BuiltInCategory.OST_Rooms)).ToElements()
     sys.exit()
 
-# ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗╔═╗
-# ║  ║  ╠═╣╚═╗╚═╗║╣ ╚═╗
-# ╚═╝╩═╝╩ ╩╚═╝╚═╝╚═╝╚═╝ CLASSES
-#==================================================
-class RoomToFloor:
-    def __init__(self, room, floor_type, active_view_level):
-        """Class for creating a floor from given room.
-        :param room:                Revit Room object.
-        :param floor_type:          selected FloorType.
-        :param active_view_level:   Level of a currently open view."""
+def select_rooms():
+    """Function to get rooms in project."""
+    selected_rooms = get_selected_rooms(uidoc, exit_if_none=False)
+    if not selected_rooms:
+        # >>>>>>>>>>Select all rooms visible in the view or exit script.
+        selected_rooms = ask_select_all_floors()
 
-        self.floor_type         = floor_type
-        self.active_view_level  = active_view_level
+    if not selected_rooms:
+        forms.alert('No rooms were selected. \nPlease Try Again.', title= __title__, exitscript=True)
+    return selected_rooms
 
-        self.room = room
-        self.area = room.get_Parameter(BuiltInParameter.ROOM_AREA).AsDouble()
+def select_floor_type():
+    """Function to display GUI and let user select Floor Type.
+    :return:  Selected FloorType """
+    all_floor_types = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Floors).WhereElementIsElementType().ToElements()
+    all_floor_types = [f for f in all_floor_types if type(f) == FloorType] # Filter ModelledInPlace Elements.
+    dict_floor_types = {Element.Name.GetValue(fr): fr for fr in all_floor_types}
+    selected_elements = select_from_dict(dict_floor_types,
+                                         title = __title__, label='Select FloorType',
+                                         button_name='Select', version=__version__,
+                                         SelectMultiple=False)
+    if selected_elements:
+        return selected_elements[0]
+    forms.alert("No FloorType was selected. \nPlease, select a FloorType and Try Again.", title=__title__, exitscript=True)
+#
 
-        if self.area:
-            self.name = room.get_Parameter(BuiltInParameter.ROOM_NAME).AsString()
-            self.room_boundaries = self.room.GetBoundarySegments(SpatialElementBoundaryOptions())
+def room_to_floor(room, floor_type):
+    # Make sure that Room is bounding.
+    if not room.get_Parameter(BuiltInParameter.ROOM_AREA).AsDouble():
+        return None
 
-            #>>>>>>>>>> CREATE Floor
-            new_floor = self.create_Floors()
+    # >>>>>>>>>> ROOM BOUNDARIES
+    room_boundaries = room.GetBoundarySegments(SpatialElementBoundaryOptions())
+    floor_shape = room_boundaries[0]
+    openings = list(room_boundaries)[1:] if len(room_boundaries) > 1 else []
+    curveLoopList = CurveArray()
+    for seg in floor_shape:
+        curveLoopList.Append(seg.GetCurve())
 
-    def create_Floors(self):
-        """Function to create a list of CurveLoop from given room boundaries."""
-        __comment__ = """Keep 2 transactions below or otherwise revit will crash without giving 
-                         any error tracebacks or whatsoever. It just shuts it down without saying anything...
-                         Just keep them unless you want some headache :) - Erik Frits"""
+    # >>>>>>>>>> CREATE FLOOR
+    with ef_Transaction(doc, 'Create Floor', debug=True):
+        new_floor = doc.Create.NewFloor(curveLoopList, floor_type, active_view_level, False)
 
-        #>>>>>>>>>> ROOM BOUNDARIES
-        floor_shape = self.room_boundaries[0]
-        openings = list(self.room_boundaries)[1:] if len(self.room_boundaries) > 1 else []
-
-        #>>>>>>>>>> CREATE FLOOR
-        with ef_Transaction(doc,'Create Floor',debug=True):
-            curveLoopList = CurveArray()
-            for seg in floor_shape:
-                curveLoopList.Append(seg.GetCurve())
-
-            new_floor = doc.Create.NewFloor(curveLoopList, self.floor_type, self.active_view_level, False)
-
-        # >>>>>>>>>> CREATE FLOOR OPENINGS
-        if openings:
-            with ef_Transaction(doc,"Create Openings"):
-                for opening in openings:
-                    opening_curve = CurveArray()
-                    for seg in opening:
-                        opening_curve.Append(seg.GetCurve())
-
-                    floor_opening = doc.Create.NewOpening(new_floor,
-                                                          opening_curve,
-                                                          True)
-        return new_floor
-
-
-class ListItem:
-    """Helper Class for displaying selected sheets in my custom GUI."""
-    def __init__(self,  Name='Unnamed', IsChecked = False, element = None):
-        self.Name       = Name
-        self.IsChecked  = IsChecked
-        self.element    = element
-
-# ╔╦╗╔═╗╦╔╗╔    ╔═╗╦ ╦╦
-# ║║║╠═╣║║║║    ║ ╦║ ║║
-# ╩ ╩╩ ╩╩╝╚╝    ╚═╝╚═╝╩ MAIN + GUI
-# ==================================================
-class MainGUI(forms.WPFWindow):
-    def __init__(self):
-        """Main class for GUI to creating floors from selected Rooms."""
-        self.selected_rooms = self.get_selected_rooms()
-        if self.selected_rooms:
-            self.form = forms.WPFWindow.__init__(self, "Script.xaml")
-            self.ListBox_FloorTypes.ItemsSource = self.generate_list_items()
-            self.main_title.Text = __title__
-            self.ShowDialog()
-
-    def create_floors(self):
-        """Function to loop through selected rooms and create floors from them."""
-        # >>>>>>>>>> LOOP THROUGH ROOMS
-        with TransactionGroup(doc, __title__) as tg:
-            tg.Start()
-            for r in self.selected_rooms:
-                room = RoomToFloor(room = r,
-                                   floor_type = self.selected_floor_type,
-                                   active_view_level = active_view_level)
-            tg.Assimilate()
-
-
-    def get_selected_rooms(self):
-        """Filter rooms from current selection.
-        If nothing is selected - ask if it should select all rooms visible in the view."""
-
-        # >>>>>>>>>> GET SELECTED ROOMS
-        selected_rooms = get_selected_rooms(uidoc, exit_if_none=False)
-        if not selected_rooms:
-            # >>>>>>>>>>Select all rooms visible in the view or exit script.
-            selected_rooms = ask_select_all_floors()
-        return selected_rooms
-
-    def generate_list_items(self):
-        """Function to create a ICollection to pass to ListBox in GUI"""
-        all_floor_types = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Floors).WhereElementIsElementType().ToElements()
-        all_floor_types = [f for f in all_floor_types if type(f) == FloorType]
-        self.dict_floor_types = {Element.Name.GetValue(fr): fr for fr in all_floor_types}
-        list_of_items = List[type(ListItem())]()
-
-        first = True
-        for type_name, floor_type in sorted(self.dict_floor_types.items()):
-            checked = True if first else False
-            first = False
-            list_of_items.Add(ListItem(type_name, checked, floor_type))
-        return list_of_items
-
-    # ╔═╗╦═╗╔═╗╔═╗╔═╗╦═╗╔╦╗╦╔═╗╔═╗
-    # ╠═╝╠╦╝║ ║╠═╝║╣ ╠╦╝ ║ ║║╣ ╚═╗
-    # ╩  ╩╚═╚═╝╩  ╚═╝╩╚═ ╩ ╩╚═╝╚═╝ PROPERTIES
-    # ==================================================
-    @property
-    def ListBox(self):
-        return self.ListBox_FloorTypes
-
-    # ╔═╗╦  ╦╔═╗╔╗╔╔╦╗  ╦ ╦╔═╗╔╗╔╔╦╗╦  ╔═╗╦═╗╔═╗
-    # ║╣ ╚╗╔╝║╣ ║║║ ║   ╠═╣╠═╣║║║ ║║║  ║╣ ╠╦╝╚═╗
-    # ╚═╝ ╚╝ ╚═╝╝╚╝ ╩   ╩ ╩╩ ╩╝╚╝═╩╝╩═╝╚═╝╩╚═╚═╝ GUI EVENT HANDLERS:
-    # ==================================================
-
-    def button_close(self,sender,e):
-        """Stop application by clicking on a <Close> button in the top right corner."""
-        self.Close()
-
-    def Hyperlink_RequestNavigate(self, sender, e):
-        """Forwarding for a Hyperlink"""
-        Start(e.Uri.AbsoluteUri)
-
-    def header_drag(self,sender,e):
-        """Drag window by holding LeftButton on the header."""
-        if e.LeftButton == MouseButtonState.Pressed:
-            DragMove(self)
-
-    def button_run(self,sender,e):
-        """Main run button.
-        Closes the dialog box and starts duplicating selected sheets."""
-        self.Close()
-
-
-        # GET SELECTED MAIN SHEET
-        selected_type_name = None
-        items = self.ListBox_FloorTypes.Items
-        for item in items:
-            if item.IsChecked:
-                selected_type_name = item.Name
-                break
-
-        self.selected_floor_type = self.dict_floor_types[selected_type_name]
-
-        # CREATE FLOORS
-        self.create_floors()
-
+    # >>>>>>>>>> CREATE FLOOR OPENINGS
+    if openings:
+        with ef_Transaction(doc, "Create Openings"):
+            for opening in openings:
+                opening_curve = CurveArray()
+                for seg in opening:
+                    opening_curve.Append(seg.GetCurve())
+                floor_opening = doc.Create.NewOpening(new_floor, opening_curve, True)
+    return new_floor
+#
+def create_floors(selected_rooms, selected_floor_type):
+    """Function to loop through selected rooms and create floors from them."""
+    # >>>>>>>>>> LOOP THROUGH ROOMS
+    with TransactionGroup(doc, __title__) as tg:
+        tg.Start()
+        for r in selected_rooms:
+            with try_except(debug=True):
+                room_to_floor(room = r, floor_type=selected_floor_type)
+        tg.Assimilate()
 
 # ╔╦╗╔═╗╦╔╗╔
 # ║║║╠═╣║║║║
 # ╩ ╩╩ ╩╩╝╚╝MAIN
 #==================================================
 if __name__ == '__main__':
-    MainGUI()
+    selected_rooms      = select_rooms()
+    selected_floor_type = select_floor_type()
+    create_floors(selected_rooms, selected_floor_type)
+
