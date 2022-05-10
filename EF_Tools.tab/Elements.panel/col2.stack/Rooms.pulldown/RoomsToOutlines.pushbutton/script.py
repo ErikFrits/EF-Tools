1648ee8bd83d75be693868b4f88758336f0f29ae
EF <erikfrits95@gmail.com>
2022-05-10 02:14:03 +0200

    📦 Updated UI + Refactored
    Updated UI:
    RadioButtons -> Checkboxes with Single Selection
    Hyperlinks updated
    Refactored code a lot.

--- EF_Tools.tab/Elements.panel/col2.stack/Rooms.pulldown/RoomsToOutlines.pushbutton/script.py
+++ EF_Tools.tab/Elements.panel/col2.stack/Rooms.pulldown/RoomsToOutlines.pushbutton/script.py
@@ -1,58 +1,55 @@
 # -*- coding: utf-8 -*-
-__title__  = "Rooms to Outline"
-__author__ = "Erik Frits"
-__doc__ = """Version = 1.0
-Date    = 16.11.2021
+__title__     = "Rooms to Outline"
+__author__    = "Erik Frits"
+__highlight__ = 'updated'
+__version__   = 'Version = 1.2'
+__context__   = 'active-floor-plan'
+__helpurl__   = 'https://www.youtube.com/watch?v=3thf8IvJVpY'
+__doc__       = """Version = 1.2
+Date    = 29.07.2021
 _____________________________________________________________________
 Description:
 
-Create room Outline from selected rooms.
-If none selected it will promt user with a Yes/No dialog box
-to select all visible rooms in an active view.
+Create Outlines with DetailedLines from selected Rooms.
+If no rooms selected it will promt user with a Yes/No dialog box 
+to select all visible Rooms in an active view.
 _____________________________________________________________________
 How-to:
--> Select rooms
+
+-> Open FloorPlan
+-> Select Rooms(it will filter other elements out)
 -> Run the script
--> If no rooms selected it ask to select all rooms in an active view.
+-> If no Rooms selected it will ask to select all Rooms in an ActiveView.
 -> Select LineStyle
 _____________________________________________________________________
 Last update:
+- [09.05.2022] - 1.2 RELEASE
+- [08.05.2022] - Updated GUI(Checkboxes + Filtering) + Refactoring
 - [17.11.2021] - 1.0 RELEASE
 _____________________________________________________________________
-Author: Erik Frits
-"""
+Author: Erik Frits"""
+
 # ╦╔╦╗╔═╗╔═╗╦═╗╔╦╗╔═╗
 # ║║║║╠═╝║ ║╠╦╝ ║ ╚═╗
 # ╩╩ ╩╩  ╚═╝╩╚═ ╩ ╚═╝ IMPORTS
 #==================================================
 import sys
-from pyrevit import revit, forms
-from Snippets._selection import get_selected_rooms
-from Snippets._context_manager import ef_Transaction
-from Autodesk.Revit.DB import (BuiltInParameter,
-                               SpatialElementBoundaryOptions,
-                               FilteredElementCollector,
-                               ElementCategoryFilter,
-                               BuiltInCategory,
-                               Element,
-                               CurveLoop,
-                               Transaction,
-                               CurveArray,
-                               CurveElement,
-                               Line,
-                               XYZ)
+from pyrevit import forms
+from Autodesk.Revit.DB import (FilteredElementCollector, ElementCategoryFilter,
+                               BuiltInCategory, BuiltInParameter,
+                               CurveArray,SpatialElementBoundaryOptions)
 
 #>>>>>>>>>> .NET IMPORTS
 import clr
-clr.AddReference("System.Windows.Forms")
-from System.Windows.Forms import (DialogResult,
-                                  MessageBox,
-                                  MessageBoxButtons)
 clr.AddReference("System")
-from System.Diagnostics.Process import Start
-from System.Collections.Generic import List
-from System.Windows.Window      import DragMove
-from System.Windows.Input       import MouseButtonState
+clr.AddReference("System.Windows.Forms")
+from System.Windows.Forms import DialogResult, MessageBox, MessageBoxButtons
+
+#Custom
+from Snippets._context_manager  import ef_Transaction, try_except
+from Snippets._selection        import get_selected_rooms
+from Snippets._lines            import get_line_styles
+from GUI.forms                  import select_from_dict
 
 # ╦  ╦╔═╗╦═╗╦╔═╗╔╗ ╦  ╔═╗╔═╗
 # ╚╗╔╝╠═╣╠╦╝║╠═╣╠╩╗║  ║╣ ╚═╗
@@ -66,180 +63,87 @@ active_view_id      = doc.ActiveView.Id
 active_view         = doc.GetElement(active_view_id)
 active_view_level   = active_view.GenLevel
 
+
 # ╔═╗╦ ╦╔╗╔╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
 # ╠╣ ║ ║║║║║   ║ ║║ ║║║║╚═╗
 # ╚  ╚═╝╝╚╝╚═╝ ╩ ╩╚═╝╝╚╝╚═╝ FUNCTIONS
 #==================================================
-def ask_select_all_floors():
+def ask_select_all_rooms():
     """Function to show Yes/No dialog box to ask user 'Incl primary and dependant views and sheets?'
     :return: List of all room visible in the currect view. if None - Exit Script"""
     # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> PROMT USER YES/NO
     dialogResult = MessageBox.Show('There were no rooms selected. Would you like to select all rooms visible in the active view?',
-                                    __title__,
-                                    MessageBoxButtons.YesNo)
-
+                                    __title__, MessageBoxButtons.YesNo)
     if (dialogResult == DialogResult.Yes):
         return FilteredElementCollector(doc, active_view_id).WherePasses(ElementCategoryFilter(BuiltInCategory.OST_Rooms)).ToElements()
     sys.exit()
 
-# ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗╔═╗
-# ║  ║  ╠═╣╚═╗╚═╗║╣ ╚═╗
-# ╚═╝╩═╝╩ ╩╚═╝╚═╝╚═╝╚═╝ CLASSES
-#==================================================
-class RoomToOutline:
-    def __init__(self, room, line_style):
-        self.line_style = line_style
-        self.room = room
-        self.area = room.get_Parameter(BuiltInParameter.ROOM_AREA).AsDouble()
-        self.room_boundaries = self.room.GetBoundarySegments(SpatialElementBoundaryOptions())
-        if not self.area:
-            return
+def select_rooms():
+    """Function to get rooms in project."""
+    selected_rooms = get_selected_rooms(uidoc, exit_if_none=False)
+    if not selected_rooms:
+        # >>>>>>>>>>Select all rooms visible in the view or exit script.
+        selected_rooms = ask_select_all_rooms()
 
-        #>>>>>>>>>> CREATE REGION
-        dl_curve_array = self.create_outlines()
+    if not selected_rooms:
+        forms.alert('No rooms were selected. \nPlease Try Again.', title= __title__, exitscript=True)
+    return selected_rooms
 
+def select_linestyle():
+    """Function to display GUI and let user select LineStyle.
+    :return:  Selected LineStyle"""
+    dict_linestyles = {ls.Name: ls for ls in all_line_styles}
+    selected_elements = select_from_dict(dict_linestyles,
+                                         title = __title__, label='Select LineStyle',
+                                         button_name='Select', version=__version__,
+                                         SelectMultiple=False)
+    if selected_elements:
+        return selected_elements[0]
+    forms.alert("No LineStyle was selected. \nPlease, Try Again.", title=__title__, exitscript=True)
 
-    @property
-    def curve_loop_list(self):
-        """Function to create a list of CurveLoop from given room boundaries."""
-        curveLoopList = []
-        for roomBoundary in self.room_boundaries:
-            room_curve_array = CurveArray()
-            for boundarySegment in roomBoundary:
-                curve = boundarySegment.GetCurve()
-                room_curve_array.Append(curve)
-            curveLoopList.Add(room_curve_array)
-        return curveLoopList
+def room_to_outline(room, line_style):
+    """Function..."""
+    #>>>>>>>>>> IGNORE NON-BOUNDING ROOMS
+    if not room.get_Parameter(BuiltInParameter.ROOM_AREA).AsDouble():
+        return None
 
-    def create_outlines(self):
-        if self.curve_loop_list:
-            # filled_region = FilledRegion.Create(doc, self.region_type_id, active_view_id, self.curve_loop_list)
-            for i in self.curve_loop_list:
-                outline = doc.Create.NewDetailCurveArray(doc.ActiveView, i) # Create Outline
+    #>>>>>>>>>> ROOM BOUNDARIES -> CurveLoopList
+    room_boundaries = room.GetBoundarySegments(SpatialElementBoundaryOptions())
+    curveLoopList = []
+    for roomBoundary in room_boundaries:
+        room_curve_array = CurveArray()
+        for boundarySegment in roomBoundary:
+            curve = boundarySegment.GetCurve()
+            room_curve_array.Append(curve)
+        curveLoopList.Add(room_curve_array)
 
+    # >>>>>>>>>> ENSURE NOT EMPTY
+    if not curveLoopList:
+        return
 
-                # #>>>>>>>>>> SET COMMENT AS ROOM NAME
-                for line in outline:
-                    line.get_Parameter(BuiltInParameter.BUILDING_CURVE_GSTYLE).Set(self.line_style.Id)
-                return outline
+    #>>>>>>>>>> CREATE OUTLINE
+    for curve in curveLoopList:
+        outline = doc.Create.NewDetailCurveArray(doc.ActiveView, curve)  # Create Outline
 
-class ListItem:
-    """Helper Class for displaying selected sheets in my custom GUI."""
-    def __init__(self,  Name='Unnamed', IsChecked = False, element = None):
-        self.Name       = Name
-        self.IsChecked  = IsChecked
-        self.element    = element
-
-# ╔╦╗╔═╗╦╔╗╔    ╔═╗╦ ╦╦
-# ║║║╠═╣║║║║    ║ ╦║ ║║
-# ╩ ╩╩ ╩╩╝╚╝    ╚═╝╚═╝╩ MAIN + GUI
-# ==================================================
-class MainGUI(forms.WPFWindow):
-    def __init__(self):
-        """Main class for GUI to creating floors from selected Rooms."""
-        self.selected_rooms = self.get_selected_rooms()
-
-        if self.selected_rooms:
-            self.form = forms.WPFWindow.__init__(self, "Script.xaml")
-            self.ListBox_FloorTypes.ItemsSource = self.generate_list_items()
-            self.main_title.Text = __title__
-            self.ShowDialog()
-
-    def create_regions(self):
-        """Function to loop through selected rooms and create floors from them."""
-        # >>>>>>>>>> LOOP THROUGH ROOMS
-        with ef_Transaction(doc, __title__, debug=True):
-            for r in self.selected_rooms:
-                new_region = RoomToOutline(room = r, line_style = self.selected_line_style)
-
-    def get_selected_rooms(self):
-        """Filter rooms from current selection.
-        If nothing is selected - ask if it should select all rooms visible in the view."""
-
-        # >>>>>>>>>> GET SELECTED ROOMS
-        selected_rooms = get_selected_rooms(uidoc, exit_if_none=False)
-        if not selected_rooms:
-            # >>>>>>>>>>Select all rooms visible in the view or exit script.
-            selected_rooms = ask_select_all_floors()
-        return selected_rooms
+        #>>>>>>>>>> CHANGE LINESTYLE
+        for line in outline:
+            line.get_Parameter(BuiltInParameter.BUILDING_CURVE_GSTYLE).Set(line_style.Id)
 
 
-    def get_line_styles(self):
-        """Function to get available LineStyles for DetaiLines."""
-        # CREATE TEMP LINE
-        t = Transaction(doc, "temp - Create DetailLine")
-        t.Start()
-        new_line         = Line.CreateBound(XYZ(0,0,0), XYZ(1,1,0))
-        random_line      = doc.Create.NewDetailCurve(active_view, new_line)
-        line_styles_ids  = random_line.GetLineStyleIds()
-        t.RollBack()
-
-        line_styles = [doc.GetElement(line_style) for line_style in line_styles_ids]
-        return line_styles
-
-    def generate_list_items(self):
-        """Function to create a ICollection to pass to ListBox in GUI"""
-
-        line_styles = self.get_line_styles()
-        self.dict_line_styles = {Element.Name.GetValue(fr): fr for fr in line_styles}
-
-        list_of_items = List[type(ListItem())]()
-
-        first = True
-        for type_name, region_type in sorted(self.dict_line_styles.items()):
-            checked = True if first else False
-            first = False
-            list_of_items.Add(ListItem(type_name, checked, region_type))
-        return list_of_items
-
-    # ╔═╗╦═╗╔═╗╔═╗╔═╗╦═╗╔╦╗╦╔═╗╔═╗
-    # ╠═╝╠╦╝║ ║╠═╝║╣ ╠╦╝ ║ ║║╣ ╚═╗
-    # ╩  ╩╚═╚═╝╩  ╚═╝╩╚═ ╩ ╩╚═╝╚═╝ PROPERTIES
-    # ==================================================
-    @property
-    def ListBox(self):
-        return self.ListBox_FloorTypes
-
-    # ╔═╗╦  ╦╔═╗╔╗╔╔╦╗  ╦ ╦╔═╗╔╗╔╔╦╗╦  ╔═╗╦═╗╔═╗
-    # ║╣ ╚╗╔╝║╣ ║║║ ║   ╠═╣╠═╣║║║ ║║║  ║╣ ╠╦╝╚═╗
-    # ╚═╝ ╚╝ ╚═╝╝╚╝ ╩   ╩ ╩╩ ╩╝╚╝═╩╝╩═╝╚═╝╩╚═╚═╝ GUI EVENT HANDLERS:
-    # ==================================================
-
-    def button_close(self,sender,e):
-        """Stop application by clicking on a <Close> button in the top right corner."""
-        self.Close()
-
-    def Hyperlink_RequestNavigate(self, sender, e):
-        """Forwarding for a Hyperlink"""
-        Start(e.Uri.AbsoluteUri)
-
-    def header_drag(self,sender,e):
-        """Drag window by holding LeftButton on the header."""
-        if e.LeftButton == MouseButtonState.Pressed:
-            DragMove(self)
-
-    def button_run(self,sender,e):
-        """Main run button.
-        Closes the dialog box and starts duplicating selected sheets."""
-        self.Close()
-
-
-        # GET SELECTED MAIN SHEET
-        selected_type_name = None
-        items = self.ListBox_FloorTypes.Items
-        for item in items:
-            if item.IsChecked:
-                selected_type_name = item.Name
-                break
-
-        self.selected_line_style = self.dict_line_styles[selected_type_name]
-
-        # CREATE FLOORS
-        self.create_regions()
+def create_outlines(selected_rooms, selected_line_style):
+    """Function to loop through selected rooms and create Outlines from them."""
+    # >>>>>>>>>> LOOP THROUGH ROOMS
+    with ef_Transaction(doc,__title__,debug=True):
+        for r in selected_rooms:
+            with try_except(debug=True):
+                room_to_outline(room = r, line_style=selected_line_style)
 
 # ╔╦╗╔═╗╦╔╗╔
 # ║║║╠═╣║║║║
 # ╩ ╩╩ ╩╩╝╚╝MAIN
 #==================================================
 if __name__ == '__main__':
-    MainGUI()
+    selected_rooms      = select_rooms()
+    all_line_styles     = get_line_styles(uidoc)
+    selected_line_style = select_linestyle()
+    create_outlines(selected_rooms, selected_line_style)
--- EF_Tools.tab/Elements.panel/col2.stack/Rooms.pulldown/RoomsToRegions.pushbutton/script.py
+++ EF_Tools.tab/Elements.panel/col2.stack/Rooms.pulldown/RoomsToRegions.pushbutton/script.py
@@ -1,60 +1,56 @@
 # -*- coding: utf-8 -*-
-__title__  = "Rooms to Regions"
-__author__ = "Erik Frits"
-__doc__ = """Version = 1.1
-# Date    = 08.02.2021
+__title__     = "Rooms to Regions"
+__author__    = "Erik Frits"
+__highlight__ = 'updated'
+__version__   = 'Version = 1.2'
+__context__   = 'active-floor-plan'
+__helpurl__   = 'https://www.youtube.com/watch?v=3thf8IvJVpY'
+__doc__ = """Version = 1.2
+Date    = 08.02.2021
 _____________________________________________________________________
-# Description:
-#
-# Create FilledRegions with selected rooms outline.
-# If none selected it will promt user with a Yes/No dialog box
-# to select all visible rooms in an active view.
-#
-# - As a bonus it will write Room's name into FilledRegion Comment
-# _____________________________________________________________________
-# How-to:
-#
-# -> Select rooms
-# -> Run the script
-# -> If no rooms selected it ask to select all rooms in an active view.
-# -> Select RegionType
+Description:
+
+Create FilledRegions with selected Rooms.
+If none selected it will promt user with a Yes/No dialog box
+to select all visible rooms in an active view.
+
+- As a bonus it will write Room's name into FilledRegion Comment
+_____________________________________________________________________
+How-to:
+
+-> Open FloorPlan
+-> Select Rooms(it will filter other elements out)
+-> Run the script
+-> If no rooms selected it ask to select all rooms in an ActiveView.
+-> Select RegionType
 _____________________________________________________________________
 Last update:
-# - [26.07.2021] - 1.0 RELEASE
-# - [26.07.2021] - Refactored
-# - [26.07.2021] - Select all rooms if None selected
-# - [26.07.2021] - Solved issue with multiple boundaries
+- [09.05.2022] - 1.2 RELEASE
+- [08.05.2022] - Updated GUI(Checkboxes + Filtering) + Refactoring
+- [26.07.2021] - 1.0 RELEASE
+- [26.07.2021] - Refactored
+- [26.07.2021] - Select all rooms if None selected
+- [26.07.2021] - Solved issue with multiple boundaries
 _____________________________________________________________________
-Author: Erik Frits
-"""
+Author: Erik Frits"""
 # ╦╔╦╗╔═╗╔═╗╦═╗╔╦╗╔═╗
 # ║║║║╠═╝║ ║╠╦╝ ║ ╚═╗
 # ╩╩ ╩╩  ╚═╝╩╚═ ╩ ╚═╝ IMPORTS
 #==================================================
 import sys
-from pyrevit import revit, forms
-from Snippets._selection import get_selected_rooms
-from Snippets._context_manager import ef_Transaction
-from Autodesk.Revit.DB import (BuiltInParameter,
-                               SpatialElementBoundaryOptions,
-                               FilteredElementCollector,
-                               ElementCategoryFilter,
-                               BuiltInCategory,
-                               Element,
-                               CurveLoop,
-                               FilledRegion, FilledRegionType)
+from pyrevit import  forms
+from Autodesk.Revit.DB import *
 
 #>>>>>>>>>> .NET IMPORTS
 import clr
-clr.AddReference("System.Windows.Forms")
-from System.Windows.Forms import (DialogResult,
-                                  MessageBox,
-                                  MessageBoxButtons)
 clr.AddReference("System")
-from System.Diagnostics.Process import Start
-from System.Collections.Generic import List
-from System.Windows.Window      import DragMove
-from System.Windows.Input       import MouseButtonState
+clr.AddReference("System.Windows.Forms")
+from System.Windows.Forms import DialogResult, MessageBox, MessageBoxButtons
+
+#Custom
+from Snippets._context_manager  import ef_Transaction
+from Snippets._selection        import get_selected_rooms
+from GUI.forms                  import select_from_dict
 
 # ╦  ╦╔═╗╦═╗╦╔═╗╔╗ ╦  ╔═╗╔═╗
 # ╚╗╔╝╠═╣╠╦╝║╠═╣╠╩╗║  ║╣ ╚═╗
@@ -72,161 +68,75 @@ active_view_level   = active_view.GenLevel
 # ╠╣ ║ ║║║║║   ║ ║║ ║║║║╚═╗
 # ╚  ╚═╝╝╚╝╚═╝ ╩ ╩╚═╝╝╚╝╚═╝ FUNCTIONS
 #==================================================
-def ask_select_all_floors():
+def ask_select_all_rooms():
     """Function to show Yes/No dialog box to ask user 'Incl primary and dependant views and sheets?'
     :return: List of all room visible in the currect view. if None - Exit Script"""
     # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>> PROMT USER YES/NO
     dialogResult = MessageBox.Show('There were no rooms selected. Would you like to select all rooms visible in the active view?',
-                                    __title__,
-                                    MessageBoxButtons.YesNo)
-
+                                    __title__, MessageBoxButtons.YesNo)
     if (dialogResult == DialogResult.Yes):
         return FilteredElementCollector(doc, active_view_id).WherePasses(ElementCategoryFilter(BuiltInCategory.OST_Rooms)).ToElements()
     sys.exit()
 
-# ╔═╗╦  ╔═╗╔═╗╔═╗╔═╗╔═╗
-# ║  ║  ╠═╣╚═╗╚═╗║╣ ╚═╗
-# ╚═╝╩═╝╩ ╩╚═╝╚═╝╚═╝╚═╝ CLASSES
-#==================================================
-class RoomToRegion:
-    def __init__(self, room, region_type):
-        self.region_type = region_type
-        self.region_type_id = region_type.Id
-        self.room = room
-        self.area = room.get_Parameter(BuiltInParameter.ROOM_AREA).AsDouble()
-        self.name = room.get_Parameter(BuiltInParameter.ROOM_NAME).AsString()
-        self.room_boundaries = self.room.GetBoundarySegments(SpatialElementBoundaryOptions())
+def select_rooms():
+    """Function to get rooms in project."""
+    selected_rooms = get_selected_rooms(uidoc, exit_if_none=False)
+    if not selected_rooms:
+        # >>>>>>>>>>Select all rooms visible in the view or exit script.
+        selected_rooms = ask_select_all_rooms()
 
-        if not self.area:
-            return
+    if not selected_rooms:
+        forms.alert('No rooms were selected. \nPlease Try Again.', title= __title__, exitscript=True)
+    return selected_rooms
 
-        #>>>>>>>>>> CREATE REGION
-        region = self.create_Regions()
+def select_region_type():
+    """Function to display GUI and let user select RegionType.
+    :return:  Selected RegionType"""
+    all_filled_regions = FilteredElementCollector(doc).OfClass(FilledRegionType)
+    dict_filled_regions = {Element.Name.GetValue(fr): fr for fr in all_filled_regions}
 
-        #>>>>>>>>>> SET COMMENT AS ROOM NAME
-        region.get_Parameter(BuiltInParameter.ALL_MODEL_INSTANCE_COMMENTS).Set(self.name)
+    selected_elements = select_from_dict(dict_filled_regions,
+                                         title = __title__, label='Select RegionType',
+                                         button_name='Select', version=__version__,
+                                         SelectMultiple=False)
+    if selected_elements:
+        return selected_elements[0]
+    forms.alert("No RegionType was selected. \nPlease, Try Again.", title=__title__, exitscript=True)
 
-    @property
-    def curve_loop_list(self):
-        """Function to create a list of CurveLoop from given room boundaries."""
-        curveLoopList = []
-        for roomBoundary in self.room_boundaries:
-            room_curve_loop = CurveLoop()
-            for boundarySegment in roomBoundary:
-                curve = boundarySegment.GetCurve()
-                room_curve_loop.Append(curve)
-            curveLoopList.Add(room_curve_loop)
-        return curveLoopList
+def create_regions(rooms, region_type):
+    """Function to loop through selected rooms and create Regions from them."""
+    # >>>>>>>>>> TRANSACTION + LOOP THROUGH ROOMS
+    with ef_Transaction(doc, __title__, debug=True):
+        for room in rooms:
 
-    def create_Regions(self):
-        if self.curve_loop_list:
-            filled_region = FilledRegion.Create(doc, self.region_type_id, active_view_id, self.curve_loop_list)
-            return filled_region
-            #doc.Create.NewDetailCurveArray(doc.ActiveView, roomCurves) # Create Outline
+            # >>>>>>>>>> IGNORE NON-BOUNDING ROOMS
+            if not room.get_Parameter(BuiltInParameter.ROOM_AREA).AsDouble():
+                return None
 
-class ListItem:
-    """Helper Class for displaying selected sheets in my custom GUI."""
-    def __init__(self,  Name='Unnamed', IsChecked = False, element = None):
-        self.Name       = Name
-        self.IsChecked  = IsChecked
-        self.element    = element
+            # >>>>>>>>>> ROOM BOUNDARIES -> CurveLoopList
+            room_boundaries = room.GetBoundarySegments(SpatialElementBoundaryOptions())
+            curveLoopList = []
 
-# ╔╦╗╔═╗╦╔╗╔    ╔═╗╦ ╦╦
-# ║║║╠═╣║║║║    ║ ╦║ ║║
-# ╩ ╩╩ ╩╩╝╚╝    ╚═╝╚═╝╩ MAIN + GUI
-# ==================================================
-class MainGUI(forms.WPFWindow):
-    def __init__(self):
-        """Main class for GUI to creating floors from selected Rooms."""
-        self.selected_rooms = self.get_selected_rooms()
-        if self.selected_rooms:
-            self.form = forms.WPFWindow.__init__(self, "Script.xaml")
-            self.ListBox_FloorTypes.ItemsSource = self.generate_list_items()
-            self.main_title.Text = __title__
-            self.ShowDialog()
+            for roomBoundary in room_boundaries:
+                room_curve_loop = CurveLoop()
+                for boundarySegment in roomBoundary:
+                    curve = boundarySegment.GetCurve()
+                    room_curve_loop.Append(curve)
+                curveLoopList.Add(room_curve_loop)
 
-    def create_regions(self):
-        """Function to loop through selected rooms and create floors from them."""
-        # >>>>>>>>>> LOOP THROUGH ROOMS
-        with ef_Transaction(doc, __title__):
-            for r in self.selected_rooms:
-                new_region = RoomToRegion(room = r, region_type = self.selected_region_type)
+            # >>>>>>>>>> CREATE REGION
+            if curveLoopList:
+                filled_region = FilledRegion.Create(doc, region_type.Id, active_view_id, curveLoopList)
 
-    def get_selected_rooms(self):
-        """Filter rooms from current selection.
-        If nothing is selected - ask if it should select all rooms visible in the view."""
-
-        # >>>>>>>>>> GET SELECTED ROOMS
-        selected_rooms = get_selected_rooms(uidoc, exit_if_none=False)
-        if not selected_rooms:
-            # >>>>>>>>>>Select all rooms visible in the view or exit script.
-            selected_rooms = ask_select_all_floors()
-        return selected_rooms
-
-    def generate_list_items(self):
-        """Function to create a ICollection to pass to ListBox in GUI"""
-
-        # >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> GET RegionType
-        all_filled_regions = FilteredElementCollector(doc).OfClass(FilledRegionType)
-        self.dict_filled_regions = {Element.Name.GetValue(fr): fr for fr in all_filled_regions}
-
-        list_of_items = List[type(ListItem())]()
-
-        first = True
-        for type_name, region_type in sorted(self.dict_filled_regions.items()):
-            checked = True if first else False
-            first = False
-            list_of_items.Add(ListItem(type_name, checked, region_type))
-        return list_of_items
-
-    # ╔═╗╦═╗╔═╗╔═╗╔═╗╦═╗╔╦╗╦╔═╗╔═╗
-    # ╠═╝╠╦╝║ ║╠═╝║╣ ╠╦╝ ║ ║║╣ ╚═╗
-    # ╩  ╩╚═╚═╝╩  ╚═╝╩╚═ ╩ ╩╚═╝╚═╝ PROPERTIES
-    # ==================================================
-    @property
-    def ListBox(self):
-        return self.ListBox_FloorTypes
-
-    # ╔═╗╦  ╦╔═╗╔╗╔╔╦╗  ╦ ╦╔═╗╔╗╔╔╦╗╦  ╔═╗╦═╗╔═╗
-    # ║╣ ╚╗╔╝║╣ ║║║ ║   ╠═╣╠═╣║║║ ║║║  ║╣ ╠╦╝╚═╗
-    # ╚═╝ ╚╝ ╚═╝╝╚╝ ╩   ╩ ╩╩ ╩╝╚╝═╩╝╩═╝╚═╝╩╚═╚═╝ GUI EVENT HANDLERS:
-    # ==================================================
-
-    def button_close(self,sender,e):
-        """Stop application by clicking on a <Close> button in the top right corner."""
-        self.Close()
-
-    def Hyperlink_RequestNavigate(self, sender, e):
-        """Forwarding for a Hyperlink"""
-        Start(e.Uri.AbsoluteUri)
-
-    def header_drag(self,sender,e):
-        """Drag window by holding LeftButton on the header."""
-        if e.LeftButton == MouseButtonState.Pressed:
-            DragMove(self)
-
-    def button_run(self,sender,e):
-        """Main run button.
-        Closes the dialog box and starts duplicating selected sheets."""
-        self.Close()
-
-
-        # GET SELECTED MAIN SHEET
-        selected_type_name = None
-        items = self.ListBox_FloorTypes.Items
-        for item in items:
-            if item.IsChecked:
-                selected_type_name = item.Name
-                break
-
-        self.selected_region_type = self.dict_filled_regions[selected_type_name]
-
-        # CREATE FLOORS
-        self.create_regions()
+                #>>>>>>>>>> SET COMMENT AS ROOM NAME
+                room_name = room.get_Parameter(BuiltInParameter.ROOM_NAME).AsString()
+                filled_region.get_Parameter(BuiltInParameter.ALL_MODEL_INSTANCE_COMMENTS).Set(room_name)
 
 # ╔╦╗╔═╗╦╔╗╔
 # ║║║╠═╣║║║║
 # ╩ ╩╩ ╩╩╝╚╝MAIN
 #==================================================
 if __name__ == '__main__':
-    MainGUI()
+    selected_rooms       = select_rooms()
+    selected_region_type = select_region_type()
+    create_regions(selected_rooms, selected_region_type)
