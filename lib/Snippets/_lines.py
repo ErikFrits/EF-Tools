d9d139e7ef559fbc7f62e757a57afb423be4b24f
EF <erikfrits95@gmail.com>
2022-05-10 02:13:00 +0200

    :rocket: lib updated
    New function:
    -get_line_styles()

--- lib/Snippets/_lines.py
+++ lib/Snippets/_lines.py
@@ -4,17 +4,26 @@
 # ║║║║╠═╝║ ║╠╦╝ ║ ╚═╗
 # ╩╩ ╩╩  ╚═╝╩╚═ ╩ ╚═╝
 #==================================================
+from Autodesk.Revit.DB import *
 
 
-
-
-
-
-
-# ╔╦╗╔═╗╦╔╗╔
-# ║║║╠═╣║║║║
-# ╩ ╩╩ ╩╩╝╚╝
+# ╦  ╦╔═╗╦═╗╦╔═╗╔╗ ╦  ╔═╗╔═╗
+# ╚╗╔╝╠═╣╠╦╝║╠═╣╠╩╗║  ║╣ ╚═╗
+#  ╚╝ ╩ ╩╩╚═╩╩ ╩╚═╝╩═╝╚═╝╚═╝ VARIABLES
 #==================================================
+uidoc   = __revit__.ActiveUIDocument
+app     = __revit__.Application
+doc     = __revit__.ActiveUIDocument.Document
+
+active_view_id      = doc.ActiveView.Id
+active_view         = doc.GetElement(active_view_id)
+active_view_level   = active_view.GenLevel
+
+# ╔═╗╦ ╦╔╗╔╔═╗╔╦╗╦╔═╗╔╗╔╔═╗
+# ╠╣ ║ ║║║║║   ║ ║║ ║║║║╚═╗
+# ╚  ╚═╝╝╚╝╚═╝ ╩ ╩╚═╝╝╚╝╚═╝ FUNCTIONS
+#==================================================
+
 def get_points_along_a_curve(curve, step=0.3):
     """ Function to get points along given Curve
     :param curve: Curve that will be tessellated.
@@ -43,3 +52,24 @@ def get_points_along_a_curve(curve, step=0.3):
         pt = q
 
     return points
+
+
+
+def get_line_styles(uidoc):
+    """Function to get available LineStyles for DetaiLines.
+    It will create a temporary DetailLine to get LineStyles to prevent having issues
+    in a project without lines.
+
+    !IMPORTANT This function has to be run outside of any transactions!
+
+    :return: list of Available LineStyles"""
+
+    # CREATE TEMP LINE
+    with Transaction(doc, "temp - Create DetailLine") as t:
+        t.Start()
+        new_line         = Line.CreateBound(XYZ(0,0,0), XYZ(1,1,0))
+        random_line      = uidoc.Document.Create.NewDetailCurve(active_view, new_line)
+        line_styles_ids  = random_line.GetLineStyleIds()
+        t.RollBack()
+    line_styles = [doc.GetElement(line_style) for line_style in line_styles_ids]
+    return line_styles
