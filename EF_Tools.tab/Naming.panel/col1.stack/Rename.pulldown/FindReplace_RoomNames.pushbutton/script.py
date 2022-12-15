2aaf1183e724e95ffa330147f7750f66f8e8de27
EF <erikfrits95@gmail.com>
2022-12-15 15:33:11 +0100

    :muscle: Improved Room Renaming Tool
    - Footer added
    - GUI Styling
    - Keep menu Open

--- EF_Tools.tab/Naming.panel/col1.stack/Rename.pulldown/FindReplace_RoomNames.pushbutton/script.py
+++ EF_Tools.tab/Naming.panel/col1.stack/Rename.pulldown/FindReplace_RoomNames.pushbutton/script.py
@@ -1,56 +1,72 @@
 # -*- coding: utf-8 -*-
 __title__ = "Find and Replace: RoomNames"
 __author__ = "Erik Frits"
-__helpurl__ = ""
+__version__ = 'Version: 1.1'
 __doc__ = """Version = 1.0
 Date    = 10.11.2020
 _____________________________________________________________________
 Description:
 
-
-Rename multiple views at once with Find/Replace/Suffix/Prefix logic.
+Rename multiple Room Names at once with Find/Replae logic.
 _____________________________________________________________________
 How-to:
 
-- Select views in ProjectBrowser
+- Select Rooms 
 - Run the script
 - Type Find/Replace/Prefix/Suffix as needed.
+- Rename Selected Rooms 
 _____________________________________________________________________
 Last update:
-
-- [10.05.2021] - 1.0 RELESE
+- [15.12.2022] - 1.1 UPDATE
+- [10.05.2021] - 1.0 RELEASE
 - [10.05.2021] - ViewSchedule, ViewDrafting added.
 - [10.05.2021] - GUI Updated
 _____________________________________________________________________
-"""
+Author: Erik Frits"""
 
 # ╦╔╦╗╔═╗╔═╗╦═╗╔╦╗╔═╗
 # ║║║║╠═╝║ ║╠╦╝ ║ ╚═╗
 # ╩╩ ╩╩  ╚═╝╩╚═ ╩ ╚═╝ IMPORTS
-#====================================================================================================
+#====================================================================
 from Autodesk.Revit.DB.Architecture import Room
 from Autodesk.Revit.DB import BuiltInParameter
 
-# ef custom
+#pyRevit
+from pyrevit import forms
+
+# Custom
 from Renaming.BaseClass_FindReplace import BaseRenaming
-from Snippets._context_manager import ef_Transaction, try_except
+from Snippets._context_manager      import ef_Transaction, try_except
+from Snippets._selection            import get_selected_elements
+
+# ╦  ╦╔═╗╦═╗╦╔═╗╔╗ ╦  ╔═╗╔═╗
+# ╚╗╔╝╠═╣╠╦╝║╠═╣╠╩╗║  ║╣ ╚═╗
+#  ╚╝ ╩ ╩╩╚═╩╩ ╩╚═╝╩═╝╚═╝╚═╝ VARIABLES
+#====================================================================
+uidoc = __revit__.ActiveUIDocument
+doc   = __revit__.ActiveUIDocument.Document
 
 # ╔═╗╦  ╔═╗╔═╗╔═╗
 # ║  ║  ╠═╣╚═╗╚═╗
 # ╚═╝╩═╝╩ ╩╚═╝╚═╝ CLASS
-#====================================================================================================
-
+#====================================================================
 class RenameRooms(BaseRenaming):
-    uidoc = __revit__.ActiveUIDocument
-    doc   = __revit__.ActiveUIDocument.Document
-    element_types = [Room]
-
     def __init__(self):
-        self.start(title=__title__)
+        self.start(title=__title__, version=__version__)
+
+    def get_selected_elements(self):
+        """Get Selected Views or let user select Views from a list."""
+        selected_elements = get_selected_elements(uidoc)
+        selected_rooms = [el for el in selected_elements if type(el) == Room]
+        if not selected_rooms:
+            forms.alert('No Rooms were selected, \nPlease Try Again', exitscript=True, title=__title__)
+
+        return selected_rooms
+
 
     def rename_elements(self):
-        """Function to rename selected Groups/GroupTypes."""
-        with ef_Transaction(self.doc,__title__):
+        """Function to rename selected Views."""
+        with ef_Transaction(doc,__title__):
             for room in self.selected_elements:
                 with try_except():
                     current_name = room.get_Parameter(BuiltInParameter.ROOM_NAME).AsString()
@@ -61,7 +77,7 @@ class RenameRooms(BaseRenaming):
 # ╔╦╗╔═╗╦╔╗╔
 # ║║║╠═╣║║║║
 # ╩ ╩╩ ╩╩╝╚╝ MAIN
-#====================================================================================================
+#====================================================================
 if __name__ == '__main__':
     x = RenameRooms()
 
