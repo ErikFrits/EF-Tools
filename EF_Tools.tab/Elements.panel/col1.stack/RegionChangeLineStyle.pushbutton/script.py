94e78dea30f991d05e0684fbc17fa34a0eecc152
EF <erikfrits95@gmail.com>
2023-02-09 12:20:41 +0100

    :bug: Change Region Linestyle
    1. Error when working in multiple projects
    2. Error If LineStyle wasnt selected

--- EF_Tools.tab/Elements.panel/col1.stack/RegionChangeLineStyle.pushbutton/script.py
+++ EF_Tools.tab/Elements.panel/col1.stack/RegionChangeLineStyle.pushbutton/script.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 __title__ = "Regions: Change Linestyle"   # Name of the button displayed in Revit
 __author__ = "Erik Frits"
-__version__ = 'Version = 1.3'
-__doc__ = """Version = 1.3
+__version__ = 'Version = 1.4'
+__doc__ = """Version = 1.4
 Date    = 18.07.2021
 _____________________________________________________________________
 Description:
@@ -16,28 +16,23 @@ How-to:
 -> Select LineStyle to apply
 _____________________________________________________________________
 Last update:
+- [07.02.2023] - 1.4 RELEASE
+    - Bug: Error when working with multiple Projects
+    - Bug: Error when no LineStyle was selected  
 - [15.12.2022] - 1.3 RELEASE
 - [24.10.2020] - 1.2 RELEASE
 - [17.11.2021] - 1.1 RELEASE
 - [17.11.2021] - Custom GUI added.
 - [18.07.2021] - 1.0 RELEASE
 _____________________________________________________________________
-Author: Erik Frits
-"""
+Author: Erik Frits"""
+
 # ╦╔╦╗╔═╗╔═╗╦═╗╔╦╗╔═╗
 # ║║║║╠═╝║ ║╠╦╝ ║ ╚═╗
 # ╩╩ ╩╩  ╚═╝╩╚═ ╩ ╚═╝ IMPORTS
 #==================================================
-from pyrevit import forms, revit
+from pyrevit import forms
 from Autodesk.Revit.DB import  *
-
-# >>>>>>>>>> .NET IMPORTS
-import clr
-clr.AddReference("System")
-from System.Diagnostics.Process import Start
-from System.Collections.Generic import List
-from System.Windows.Window import DragMove
-from System.Windows.Input import MouseButtonState
 
 #>>>>>>>>>> CUSTOM IMPORTS
 from Snippets._selection import get_selected_elements
@@ -80,8 +75,14 @@ valid_line_styles_ids = region.GetValidLineStyleIdsForFilledRegion(doc)
 valid_line_styles = [doc.GetElement(i) for i in valid_line_styles_ids]
 dict_valid_line_styles = {i.Name: i for i in valid_line_styles}
 
-selected_line_style = select_from_dict(dict_valid_line_styles, title=__title__, label='Select LineStyle', SelectMultiple=False, version=__version__)[0]
+# ASK FOR USER INPUT (select LineStyle)
+selected_line_style = select_from_dict(dict_valid_line_styles, title=__title__, label='Select LineStyle', SelectMultiple=False, version=__version__)
+if selected_line_style and type(selected_line_style) == list:
+    selected_line_style = selected_line_style[0]
+else:
+    forms.alert('No LineStyle was selected. Please try Again', title=__title__ ,exitscript=True)
 
+# CHANGE LINESTYLE
 with ef_Transaction(doc, __title__):
     for region in selected_filled_regions:
         with try_except():
--- lib/Snippets/_selection.py
+++ lib/Snippets/_selection.py
@@ -36,11 +36,15 @@ selection = uidoc.Selection                          # type: Selection
 #==================================================
 def get_selected_elements(uidoc = uidoc, exitscript=True):
     """Property that retrieves selected views or promt user to select some from the dialog box."""
-    doc               = uidoc.Document
-    selected_elements = [doc.GetElement(e_id) for e_id in selection.GetElementIds()]
+    doc       = uidoc.Document
+    selection = uidoc.Selection  # type: Selection
 
-    if not selected_elements:
-        forms.alert("No elements  were selected.\nPlease, try again.", exitscript=exitscript)
+    try:
+        selected_elements = [doc.GetElement(e_id) for e_id in selection.GetElementIds()]
+        if not selected_elements:
+            forms.alert("No elements  were selected.\nPlease, try again.", exitscript=exitscript)
+    except:
+        return
 
     return selected_elements
 
@@ -49,7 +53,9 @@ def get_selected_elements(uidoc = uidoc, exitscript=True):
 def get_selected_rooms(uidoc=uidoc, exitscript = True):
     """Function to Pick Rooms.
     Previously selected rooms will be pre-selected."""
-    doc               = uidoc.Document
+    doc       = uidoc.Document
+    selection = uidoc.Selection  # type: Selection
+
     selected_elements = [doc.GetElement(e_id) for e_id in selection.GetElementIds()]
     selected_rooms    = [e for e in selected_elements if type(e) == Room]
     ref_rooms         = [Reference(r) for r in selected_rooms]
