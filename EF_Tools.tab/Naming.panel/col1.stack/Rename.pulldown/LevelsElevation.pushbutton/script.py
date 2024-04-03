deed3cacfd98ad04ded54187739c3e545a889d17
Erik Frits <erikfrits95@gmail.com>
2024-04-03 17:53:25 +0200

    ⌨️ Refactored imported function

--- EF_Tools.tab/Naming.panel/col1.stack/Rename.pulldown/LevelsElevation.pushbutton/script.py
+++ EF_Tools.tab/Naming.panel/col1.stack/Rename.pulldown/LevelsElevation.pushbutton/script.py
@@ -37,7 +37,9 @@ from pyrevit import forms
 from GUI.forms                  import my_WPF
 from Snippets._convert          import convert_internal_to_m
 from Snippets._context_manager  import ef_Transaction, try_except
-from Snippets._text             import get_text_in_brackets
+
+
+
 
 #>>>>>>>>>> .NET IMPORTS
 import clr
@@ -51,6 +53,19 @@ import wpf
 #==================================================
 doc   = __revit__.ActiveUIDocument.Document
 PATH_SCRIPT = os.path.dirname(__file__)
+
+def get_text_in_brackets(text, symbol_start, symbol_end):
+    """Function to get contents between 2 symbols.
+    :param text:            Given Text
+    :param symbol_start:
+    :param symbol_end:
+    :return:
+    e.g.
+    get_text_in_brackets('This is [not] important message', '[', ']')
+    => 'not'"""
+    start = text.find(symbol_start) + len(symbol_start) if symbol_start in text else None
+    stop = text.find(symbol_end) if symbol_end in text else None
+    return str(text[start:stop])
 
 # ╔═╗╦  ╔═╗╔═╗╔═╗
 # ║  ║  ╠═╣╚═╗╚═╗
